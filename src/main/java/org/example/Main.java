14ec530ccf4bc9ba80960c12d87fda66ea5dcc0d
Z0ro0 <w2110@e-mirim.hs.kr>
2023-04-20 11:16:00 +0900

    study lombok 설치 확인

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,12 @@
 package org.example;
 
+import lombok.Data;
+
+@Data
+class Hello{
+
+}
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
